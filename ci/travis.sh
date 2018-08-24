0c0f415f3a46aaa8faeba2e9abafa61ee5cff92c
Andrew Chambers <andrewchambers@fastmail.com>
2018-08-24 18:28:09 +1200

    work on suite

--- ci/travis.sh
+++ ci/travis.sh
@@ -12,17 +12,16 @@ cleanup () {
 }
 trap cleanup EXIT
 
-if ! test -d 9cc-git
+# Get latest 9cc version.
+if ! test -d 9cc_git
 then
-    git clone https://github.com/rui314/9cc 9cc-git
-    cd 9cc-git
+    git clone https://github.com/rui314/9cc 9cc_git
+    cd 9cc_git
 else
-    cd 9cc-git
+    cd 9cc_git
     git fetch --all
     git reset --hard origin/master
 fi
-
-commit9cc=$(git rev-parse HEAD)
 
 git clean -fxd
 if ! make
@@ -32,29 +31,54 @@ fi
 export PATH="$(pwd)":$PATH
 cd ..
 
-testrunname9cc="9cc-simple-exec-$testrundate-$commit9cc.tap"
-results9cc="$scratchdir/$testrunname9cc"
-./simple-exec 9cc | tee "$results9cc"
-
+# Run tests for each, generating html
 test -d && rm -rf ./output_html
 mkdir output_html
 
+
 cat <<EOF > ./output_html/index.html
 <html>
-<header><title>c-test-suite</title></header>
+<header><title>ctest suite</title></header>
 <body>
-EOF
-
-echo "<a href=\"/$testrunname9cc.txt\">$testrunname9cc</a>" >> ./output_html/index.html
-cp $results9cc ./output_html/$testrunname9cc.txt
-echo "<pre>" >> ./output_html/index.html
-./scripts/tapsummary < $results9cc >> ./output_html/index.html
-echo "</pre>" >> ./output_html/index.html
-
-cat <<EOF >> ./output_html/index.html
+<h1>ctest-suite daily runner</h1>
+<a href="https://github.com/rui314/9cc">9cc</a>
+<a href="/9cc_latest.html">latest test results</a>
+<br>
+<a href="TODO">scc</a>
+<a href="/scc_latest.html">latest test results</a>
+last updated: $testrundate
+<br>
 </body>
 </html>
 EOF
+
+for compiler in 9cc
+do
+    commit="$(cd ${compiler}_git && git rev-parse HEAD)"
+    testrunname="$compiler-simple-exec-$testrundate-$commit.tap"
+    results="$scratchdir/$testrunname"
+    ./simple-exec 9cc | tee "$results"
+
+    htmlfile="./output_html/${compiler}_latest.html"
+
+    cat <<EOF > "$htmlfile"
+    <html>
+    <header><title>c-test-suite</title></header>
+    <body>
+EOF
+    echo "<h2>$testrunname</h2>" >> "$htmlfile"
+    echo "<a href=\"/${compiler}_latest.tap.txt\">$testrunname</a>" >> "$htmlfile"
+    cp $results "./output_html/$testrunname.txt"
+    echo "<pre>" >> "$htmlfile"
+    ./scripts/tapsummary < $results >> "$htmlfile"
+    echo "</pre>" >> "$htmlfile"
+
+    cat <<EOF >> "$htmlfile"
+    </body>
+    </html>
+EOF
+
+done
 
 
 set +x
