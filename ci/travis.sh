fe539cdf57cf9bb8402b2bfd794c689e2ac58a4d
Andrew Chambers <andrewchambers@fastmail.com>
2018-08-25 15:39:34 +1200

    fix ccgo version

--- ci/travis.sh
+++ ci/travis.sh
@@ -47,7 +47,7 @@ go env
 go get -v -u github.com/cznic/ccgo/v2/...
 go get -v -u github.com/cznic/crt
 export PATH=$HOME/go/bin:$PATH
-ccgo --version > ccgo_version.txt 2>&1
+(cd $HOME/go/src/github.com/cznic/ccgo/ && git rev-parse HEAD) > ccgo_version.txt
 
 # install tcc
 nix-env -i tcc
